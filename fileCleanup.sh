071ce0d611b58ef18c976157cc3feecd00fcf0ec
Kyle Rosauer <152750819+Krosauer@users.noreply.github.com>
2024-08-15 21:07:20 -0500

    Update fileCleanup.sh
    Fix bugs

--- fileCleanup.sh
+++ fileCleanup.sh
@@ -3,8 +3,11 @@
 cd /Users/kylerosauer/Downloads
 for ext in `find *  -maxdepth 1 -type f | rev | cut -d '.' -f1 | rev | sort -u`
 do
-
-  mkdir -p $ext
-  mv *.$ext $ext
-
+  for file in `ls *.${ext}`
+	do
+      if ! [[ -d ${file} ]]; then
+        mkdir -p ${ext}
+        mv ${file} ${ext}
+      fi
+	done
 done
